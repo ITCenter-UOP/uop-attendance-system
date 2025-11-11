5631978a065d3eb5f39147cebc172402e056fe72
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-11-11 09:13:20 +0530

    Update

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -7,29 +7,29 @@ import ForgetPassword from '../pages/AuthPages/ForgetPassword'
 import VerifyEmail from '../pages/AuthPages/VerifyEmail'
 import VerifyOTP from '../pages/AuthPages/VerifyOTP'
 import UpdatePassword from '../pages/AuthPages/UpdatePassword'
-
+import Dashboard from '../layouts/Dashboard'
+import DashError from '../component/Errors/DashError'
+import PrivateRoute from './PrivateRoute'
+import DashHome from '../pages/Dashboard/DashHome'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* website */}
                 <Route path='/' element={<WebSite />} >
                     <Route path='*' element={<DefultError />} />
-                    <Route index element={<Login />} /> 
-                    <Route path='create-account' element={<CreateAccount /> } />
-                    <Route path='forget-password' element={<ForgetPassword /> } />
-                    <Route path='verify-account' element={<VerifyEmail /> } />
-                    <Route path='verify-otp' element={<VerifyOTP /> } />
-                    <Route path='update-password' element={<UpdatePassword /> } />
-                
-
-                    {/* <Route path='create-account' element={<CreateAccount />} />
-                    <Route path='verify-email' element={<VerifyEmail />} />
-                    <Route path='login' element={<Login />} />
+                    <Route index element={<Login />} />
+                    <Route path='create-account' element={<CreateAccount />} />
                     <Route path='forget-password' element={<ForgetPassword />} />
+                    <Route path='verify-account' element={<VerifyEmail />} />
                     <Route path='verify-otp' element={<VerifyOTP />} />
-                    <Route path='update-password' element={<UpdatePassword />} /> */}
+                    <Route path='update-password' element={<UpdatePassword />} />
+
+                    <Route path='/Dashboard' element={<PrivateRoute roles={['admin', 'supervisor']}><Dashboard /></PrivateRoute>}>
+                        <Route path='*' element={<PrivateRoute roles={['admin', 'supervisor']}><DashError /> </PrivateRoute>} />
+                        <Route index element={<PrivateRoute roles={['admin', 'supervisor']}><DashHome /> </PrivateRoute>} />
+                    </Route>
+
                 </Route>
 
             </Routes>
